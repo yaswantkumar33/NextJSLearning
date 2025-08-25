a4b8f6ca6776edcd280b79771d61673f45716b08
articstark <yaswantkumar1512@gmail.com>
2025-08-25 22:55:21 +0530

    get req data completed in nextjs

--- src/app/api/items/route.js
+++ src/app/api/items/route.js
@@ -1,6 +1,15 @@
-export async function GET() {
+import { log } from "console";
+
+export async function GET(req) {
+  let reqdataget = req.nextUrl.searchParams.get("type");
+  log(reqdataget); //you get all your req data in this req
   return Response.json([{ NAME: "item1" }, { NAME: "item2" }]);
 }
-export async function POST() {
-  return Response.json([{ NAME: "itemPost1" }, { NAME: "itemPost2" }]);
+export async function POST(req) {
+  let reqdatapost = await req.json();
+  console.log(reqdatapost);
+
+  const responseData = [{ NAME: "itemPost1" }, { NAME: "itemPost2" }];
+
+  return Response.json(responseData);
 }
